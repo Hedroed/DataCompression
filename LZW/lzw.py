25453c3d0b33ac9d2655f4d92d61ce97cd510754
Rydin Nathan <nathan.rydin@orange.com>
2018-05-03 17:57:59 +0200

    Fix bug when compress same charactere multiple time

--- LZW/lzw.py
+++ LZW/lzw.py
@@ -10,8 +10,8 @@
 def compress(text: bytes):
 
     groups = []  # Dictionnary
-    length = 8
-    offset = pow(2, length)
+    length = 9
+    offset = pow(2, length-1)
 
     ret = ''
 
@@ -27,27 +27,27 @@ def compress(text: bytes):
             w += current
             current = text[i:i+1]
             i += 1
-        # print(current)
 
-        # print(w)
         if len(w) == 1:
-            r = "{:b}".format(int.from_bytes(w, 'big')).rjust(length+1, '0')
+            r = "{:b}".format(int.from_bytes(w, 'big')).rjust(length, '0')
         else:
-            r = "{:b}".format(groups.index(w) + 256).rjust(length+1, '0')
+            r = "{:b}".format(groups.index(w) + 256).rjust(length, '0')
 
+        # print("\nw: %s , c: %s" % (w, current))
+        # print('Add %s (%d) [%d]' % (r, int(r, 2), len(groups)))
         ret += r
 
         if len(groups) >= offset:
-            print('Size up')
-            length += 1
+            # print('Size up')
             offset = pow(2, length)
+            length += 1
 
         if i < textLen:
             groups.append(w + current)
+        # print(groups)
 
         w = current
 
-    # print("padding len", 8 - len(ret) % 8)
     ret += '0' * (8 - len(ret) % 8)  # padding with 0
 
     bitcode = [int(ret[i:i+8], 2) for i in range(0, len(ret), 8)]
@@ -58,8 +58,6 @@ def decompress(text: bytes):
 
     bitcode = ''.join("{:08b}".format(i) for i in text)
     # print(bitcode)
-    # code = [int(bitcode[i:i+9], 2) for i in range(0, len(bitcode), 9)]
-    # print(code)
 
     groups = []  # Dictionnary
     length = 9
@@ -71,26 +69,28 @@ def decompress(text: bytes):
 
     bitLen = len(bitcode)
     i = length
+    r = b''
     while i+length <= bitLen:
 
-        # print(bitcode[i:i+length])
-
-        v = int(bitcode[i:i+length], 2)  # current code
+        bc = bitcode[i:i+length]
+        v = int(bc, 2)  # current code
         i += length
 
-        print(v, len(groups))
+        # print("\nGet %s (%d) [%d]" % (bc, v, len(groups)))
 
-        r = b''
         if v < 256:
             r = v.to_bytes(1, 'big')
+        elif v - 256 >= len(groups):
+            r = w + r[0:1]
         else:
             r = groups[v - 256]
 
         ret += r
         groups.append(w + r[0:1])
+        # print(groups)
 
         if len(groups) >= offset:
-            print('Size up')
+            # print('Size up')
             offset = pow(2, length)
             length += 1
 
