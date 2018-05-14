b31a9b98d1de5b8b2e5aad128f7b9b4cb51f74c8
Mathieu LUX <mathieu.lux@orange.com>
2018-05-14 20:43:32 +0200

    Add huffman tests with additionals infos

--- Huffman/test_huffman.py
+++ Huffman/test_huffman.py
@@ -2,12 +2,10 @@
 # coding: utf-8
 
 from huffman import *
+import time
 
 
-def average():
-
-    F = frequencies()
-    tree = huffman_tree(F)
+def average(tree):
     code = huffman_code(tree)
 
     avg = 0
@@ -17,23 +15,77 @@ def average():
     return avg/len(code) * 100
 
 
-def test_compress():
+def compression_ratio(uncompressed, compressed):
+    return 1 - len(compressed) / len(uncompressed) * 100
 
-    F = frequencies()
-    tree = huffman_tree(F)
-    code = huffman_code(tree)
 
-    compressed = compress_file(code, "samples/horla.txt")
-    print(compressed)
+def test_compress_decompress_horla():
+
+    ## Unit test
+    with open("samples/horla.txt") as f:
+        original_data = f.read()
+
+    t1 = time.time()
+    compressed_data, tree = compress_file("samples/horla.txt", "samples/horla.txt.huf")
+    t2 = time.time()
+    decompress_data = decompress_file(tree, "samples/horla.txt.huf")
+    t3 = time.time()
+
+    assert original_data == decompress_data
+
+    ## Additional infos (pytest -s)
+    print('\n[i] Compression of samples/horla.txt')
+    print("->  File size: %d Bytes" % len(original_data.encode()))
+    print("->  Compress time: %.4fs" % (t2 - t1))
+    print("->  Decompress time: %.4fs" % (t3 - t2))
+    print("->  Compression ratio: %.2f%%" % compression_ratio(original_data, compressed_data))
+
+
+def test_compress_decompress_french_text():
+
+    ## Unit test
+    with open("samples/french_text.txt") as f:
+        original_data = f.read()
+
+    t1 = time.time()
+    compressed_data, tree = compress_file("samples/french_text.txt", "samples/french_text.txt.huf")
+    t2 = time.time()
+    decompress_data = decompress_file(tree, "samples/french_text.txt.huf")
+    t3 = time.time()
+
+    assert original_data == decompress_data
+
+    ## Additional infos (pytest -s)
+    print('\n[i] Compression of samples/french_text.txt')
+    print("->  File size: %d KB" % (len(original_data.encode()) / 1024))
+    print("->  Compress time: %.4fs" % (t2 - t1))
+    print("->  Decompress time: %.4fs" % (t3 - t2))
+    print("->  Compression ratio: %.2f%%" % compression_ratio(original_data, compressed_data))
+
+
+def test_compress_decompress_message():
+
+    ## Unit test
+    with open("samples/message.txt") as f:
+        original_data = f.read()
+
+    t1 = time.time()
+    compressed_data, tree = compress_file("samples/message.txt", "samples/message.txt.huf")
+    t2 = time.time()
+    decompress_data = decompress_file(tree, "samples/message.txt.huf")
+    t3 = time.time()
+
+    assert original_data == decompress_data
+
+    ## Additional infos (pytest -s)
+    print('\n[i] Compression of samples/message.txt')
+    print("->  File size: %d Bytes" % len(original_data.encode()))
+    print("->  Compress time: %.4fs" % (t2 - t1))
+    print("->  Decompress time: %.4fs" % (t3 - t2))
+    print("->  Compression ratio: %.2f%%" % compression_ratio(original_data, compressed_data))
 
 
 if __name__ == '__main__':
-    # print("Code length average : %s" % average())
-    test_compress()
-
-    F = frequencies()
-    tree = huffman_tree(F)
-    code = huffman_code(tree)
-
-    content = decompress_file(tree, "samples/horla.txt.huf")
-    print(content)
+    test_compress_decompress_horla()
+    test_compress_decompress_french_text()
+    test_compress_decompress_message()
